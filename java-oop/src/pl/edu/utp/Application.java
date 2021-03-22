e0b3ae84612267fb2a55b04d7e52f9adc59454ca
Stan <Stan@tata-vaio>
2021-03-22 16:02:58 +0100

    send br1

---
+++ java-oop/src/pl/edu/utp/Application.java
@@ -0,0 +1,10 @@
+package pl.edu.utp;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world....");
+	}
+
+}
